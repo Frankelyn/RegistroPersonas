ca502012a91530cfcd05ac39b4f27cd7bdfea42d
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-05-13 14:46:04 -0400

    Reparando buildscript

--- app/src/main/java/com/ejemplo/registropersonas/viewmodel/PersonaViewModel.kt
+++ app/src/main/java/com/ejemplo/registropersonas/viewmodel/PersonaViewModel.kt
@@ -7,12 +7,17 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 import com.ejemplo.registropersonas.model.Persona
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+
 
 @HiltViewModel
 class PersonaViewModel @Inject constructor(
     val personasDao: PersonasDao
 ) : ViewModel(){
+
+    val personas : Flow<List<Persona>>
+        get() =  personasDao.GetLista()
 
     private val _guardado = MutableLiveData(false)
     val guardado: LiveData<Boolean> get() = _guardado
